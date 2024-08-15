35776b427bb9b035f7940e136bc698997f51b8ff
phillipjeromeyosief <phillip.yosief@outlook.com>
2024-08-15 13:55:17 +0200

    init

---
+++ setup.py
@@ -0,0 +1,64 @@
+import os.path
+
+from setuptools import find_packages, setup
+
+
+def read(rel_path: str) -> str:
+    here = os.path.abspath(os.path.dirname(__file__))
+    # intentionally *not* adding an encoding option to open
+    with open(os.path.join(here, rel_path)) as fp:
+        return fp.read()
+
+
+def get_version(rel_path: str) -> str:
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            # __version__ = "0.9"
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="macOS_menubar",
+    version=get_version("macOS-menubar/version.py"),
+    description="A simple way to create a native macOS menubar as an addition to your python application.",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+    url="https://github.com/phillipyosief/macOS-menubar",
+    author="Phillip Jerome Yosief",
+    author_email="phillip.yosief@outlook.com",
+    install_requires=[
+
+    ],
+    extras_require={
+        "dev": [
+            "check-manifest",
+            "twine",
+            "black",
+        ],
+    },
+)
